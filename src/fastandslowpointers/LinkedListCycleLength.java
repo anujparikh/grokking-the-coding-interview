6fd918989ad4be042d5c6ed22fb92766df58e1d4
Anuj Parikh <anujparikh1@gmail.com>
2022-02-26 15:47:23 -0600

    Length of cycle in Linkedlist

---
+++ src/fastandslowpointers/LinkedListCycleLength.java
@@ -0,0 +1,39 @@
+package fastandslowpointers;
+
+import utils.ListNode;
+
+public class LinkedListCycleLength {
+    public static int findCycleLength(ListNode head) {
+        ListNode slowPointer = head;
+        ListNode fastPointer = head;
+
+        while (fastPointer != null && fastPointer.next != null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+            if (slowPointer == fastPointer) {
+                int size = 1;
+                ListNode sizePointer = slowPointer.next;
+                while (sizePointer != slowPointer) {
+                    size++;
+                    sizePointer = sizePointer.next;
+                }
+                return size;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+        head.next.next.next.next.next.next = head.next.next;
+        System.out.println("LinkedList cycle length: " + LinkedListCycleLength.findCycleLength(head));
+
+        head.next.next.next.next.next.next = head.next.next.next;
+        System.out.println("LinkedList cycle length: " + LinkedListCycleLength.findCycleLength(head));
+    }
+}
