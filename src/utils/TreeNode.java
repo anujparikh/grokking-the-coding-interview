a23ad2eb84f63573788b9834cc43ccb912d5db4a
Anuj Parikh <anujparikh1@gmail.com>
2022-03-01 16:47:57 -0600

    Maximum Depth of Binary Tree

---
+++ src/utils/TreeNode.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class TreeNode {
+    public int val ;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode() {};
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
